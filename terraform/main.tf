7b1db50390a9ba9dcb04fd9c301e85fc09daddc8
prudhviaddepalli <115047399+prudhviaddepalli@users.noreply.github.com>
2023-11-28 07:28:54 -0600

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,29 @@
 
+terraform {
+  required_version = ">= 0.14.0"
+  required_providers {
+    confluent = {
+      source  = "confluentinc/confluent"
+      version = "1.55.0"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.55.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "your-resourcegroup"
+    storage_account_name = "storageaccountname"
+    container_name       = "containername"
+    key                  = "devcluster.terraform.tfstate"
+  }
+}
+
+provider "confluent" {
+  cloud_api_key    = var.confluent_cloud_api_key
+  cloud_api_secret = var.confluent_cloud_api_secret
+}
+
+resource "confluent_environment" "staging" {
+  display_name = "terraformcicd"
+}
