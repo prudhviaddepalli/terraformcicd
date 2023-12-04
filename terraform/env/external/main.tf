39d8a1a50e55d5b2bf2a0afb4631666f0c5c7fdb
prudhviaddepalli <115047399+prudhviaddepalli@users.noreply.github.com>
2023-12-03 18:53:55 -0600

    Update main.tf

--- terraform/env/external/main.tf
+++ terraform/env/external/main.tf
@@ -1,3 +1,21 @@
+terraform {
+  required_version = ">= 0.14.0"
+  required_providers {
+    confluent = {
+      source  = "confluentinc/confluent"
+      version = "1.55.0"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.55.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "your-resourcegroup"
+    storage_account_name = "storageaccountname"
+    container_name       = "containername"
+  }
+}
 
 resource "confluent_kafka_cluster" "standard" {
   display_name = "staging-db"
