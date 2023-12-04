8f5951caca386f1282ec3bc9acc47b7e745d89bc
prudhviaddepalli <115047399+prudhviaddepalli@users.noreply.github.com>
2023-12-03 20:59:52 -0600

    Update output.tf

--- terraform/env/output.tf
+++ terraform/env/output.tf
@@ -1,8 +1,8 @@
 output "resource-ids" {
-  
+  value = <<-EOT
   Environment ID:   ${confluent_environment.staging.id}
   Schema Registry URL: ${confluent_schema_registry_cluster.essentials.rest_endpoint}
-
+  EOT
   
 
   sensitive = false
