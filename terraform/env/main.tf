3dc9f2261bc885d2ba812e7e022f199ea2c886a5
prudhviaddepalli <115047399+prudhviaddepalli@users.noreply.github.com>
2023-12-03 20:50:45 -0600

    Update main.tf

--- terraform/env/main.tf
+++ terraform/env/main.tf
@@ -25,5 +25,25 @@ resource "confluent_environment" "staging" {
   display_name = var.envname
 }
 
+data "confluent_schema_registry_region" "essentials" {
+  cloud   = "AZURE"
+  region  = "westus2"
+  package = "ESSENTIALS"
+}
+
+resource "confluent_schema_registry_cluster" "essentials" {
+  package = data.confluent_schema_registry_region.essentials.package
+
+  environment {
+    id = confluent_environment.staging.id
+  }
+
+  region {
+    # See https://docs.confluent.io/cloud/current/stream-governance/packages.html#stream-governance-regions
+    # Stream Governance and Kafka clusters can be in different regions as well as different cloud providers,
+    # but you should to place both in the same cloud and region to restrict the fault isolation boundary.
+    id = data.confluent_schema_registry_region.essentials.id
+  }
+}
 
 
