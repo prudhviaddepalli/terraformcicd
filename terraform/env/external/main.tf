3ce46cf7721ef4ac896aba96415e04f1b5a39e7b
prudhviaddepalli <115047399+prudhviaddepalli@users.noreply.github.com>
2023-12-03 18:53:34 -0600

    Update main.tf

--- terraform/env/external/main.tf
+++ terraform/env/external/main.tf
@@ -1,1 +1,11 @@
 
+resource "confluent_kafka_cluster" "standard" {
+  display_name = "staging-db"
+  availability = "SINGLE_ZONE"
+  cloud        = "GCP"
+  region       = "us-central1"
+  basic {}
+  environment {
+    id = confluent_environment.staging.id
+  }
+}
