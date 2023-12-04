c61faa3725cdd009ee57586ca4156362c657b096
prudhviaddepalli <115047399+prudhviaddepalli@users.noreply.github.com>
2023-12-03 18:54:28 -0600

    Update main.tf

--- terraform/env/internal/main.tf
+++ terraform/env/internal/main.tf
@@ -1,12 +1,29 @@
-resource "confluent_network" "private-link" {
-  display_name     = "Private Link Network"
-  cloud            = "AZURE"
-  region           = var.region
-  connection_types = ["PRIVATELINK"]
+terraform {
+  required_version = ">= 0.14.0"
+  required_providers {
+    confluent = {
+      source  = "confluentinc/confluent"
+      version = "1.55.0"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.55.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "your-resourcegroup"
+    storage_account_name = "storageaccountname"
+    container_name       = "containername"
+  }
+}
+
+resource "confluent_kafka_cluster" "standard" {
+  display_name = var.clustername
+  availability = "SINGLE_ZONE"
+  cloud        = "GCP"
+  region       = "us-central1"
+  basic {}
   environment {
     id = confluent_environment.staging.id
-  }
-  dns_config {
-    resolution = "PRIVATE"
   }
 }
