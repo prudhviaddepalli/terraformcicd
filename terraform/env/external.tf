f900600da7f0f61fe815e7c2107bf356e94bb564
prudhviaddepalli <115047399+prudhviaddepalli@users.noreply.github.com>
2023-12-03 22:20:34 -0600

    Update external.tf

--- terraform/env/external.tf
+++ terraform/env/external.tf
@@ -8,3 +8,34 @@ resource "confluent_kafka_cluster" "external" {
     id = confluent_environment.staging.id
   }
 }
+
+
+resource "confluent_service_account" "externalclusteradmin" {
+  display_name = var.saexternalclusteradmin
+  description  = "Service account to manage 'inventory' Kafka cluster"
+}
+
+resource "confluent_role_binding" "app-manager-kafka-cluster-admin" {
+  principal   = "User:${confluent_service_account.externalclusteradmin.id}"
+  role_name   = "CloudClusterAdmin"
+  crn_pattern = confluent_kafka_cluster.external.rbac_crn
+}
+
+resource "confluent_api_key" "app-manager-kafka-api-key" {
+  display_name = "app-manager-kafka-api-key"
+  description  = "Kafka API Key that is owned by 'app-manager' service account"
+  owner {
+    id          = confluent_service_account.externalclusteradmin.id
+    api_version = confluent_service_account.externalclusteradmin.api_version
+    kind        = confluent_service_account.externalclusteradmin.kind
+  }
+
+  managed_resource {
+    id          = confluent_kafka_cluster.external.id
+    api_version = confluent_kafka_cluster.external.api_version
+    kind        = confluent_kafka_cluster.external.kind
+
+    environment {
+      id = confluent_environment.staging.id
+    }
+  }
