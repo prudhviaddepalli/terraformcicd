3deff858dc324f4ad88da1b371f3a40ce598424d
prudhviaddepalli <115047399+prudhviaddepalli@users.noreply.github.com>
2023-12-03 21:58:12 -0600

    Update output.tf

--- terraform/env/output.tf
+++ terraform/env/output.tf
@@ -5,3 +5,11 @@ output "environment_id" {
 output "schemaregistry_url" {
   value = confluent_schema_registry_cluster.essentials.rest_endpoint
 }
+
+output "Internal_cluster_id" {
+  value = confluent_kafka_cluster.internal.id
+}
+
+output "External_cluster_id" {
+  value = confluent_kafka_cluster.external.id
+}
